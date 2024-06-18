89464df7f26e25a8279eb6d887bf5ef66e929092
Byron Zhong <zhongbangyuan@gmail.com>
2024-06-17 22:41:39 -0500

    starter code for hw1

---
+++ hw1/color.c
@@ -0,0 +1,15 @@
+/*
+ * color.c
+ *
+ * YOUR CODE HERE
+ */
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(int argc, char *argv[])
+{
+    (void) argc, argv; // REMOVE THIS LINE
+
+    /* the program terminates normally */
+    return EXIT_SUCCESS;
+}
