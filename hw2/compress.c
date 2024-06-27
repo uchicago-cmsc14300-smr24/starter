d87b25351bb44df6c31cb3abf994e837ea44e790
Byron Zhong <zhongbangyuan@gmail.com>
2024-06-27 15:36:14 -0500

    update missing header

--- hw2/compress.c
+++ hw2/compress.c
@@ -12,10 +12,11 @@
 #include "majors.h"
 
 #include <assert.h>
+#include <stdbool.h>
+#include <stdint.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#include <stdbool.h>
 
 /* Represents a row of student record */
 struct student {
